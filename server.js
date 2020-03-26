30e4c8027d7e3b2eb17aa72b834587e82801d57a
SrijitaSarkar99 <srijitasarkar9912@gmail.com>
2020-03-26 17:34:08 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+//Connect Datebase
+connectDB();
+
 app.get('/', (req,res) => res.send('API Running'));
+
+//DEfine routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/post', require('./routes/api/post'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
