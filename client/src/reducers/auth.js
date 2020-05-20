71e1074bc40da1740e3d61b1dc1ca0917bc943fa
SrijitaSarkar99 <srijitasarkar9912@gmail.com>
2020-05-20 19:15:55 +0530

    login error

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,6 +1,10 @@
 import {
   REGISTER_SUCCESS,
-  REGISTER_FAIL
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
 } from '../actions/types';
 
 const initialState = {
@@ -8,29 +12,39 @@ const initialState = {
   isAthenticated: null,
   loading: true,
   user: null
-}
+};
 
 export default function(state = initialState, action) {
   const { type, payload } = action;
 
   switch(type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload
+      };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', payload.token);
       return {
         ...state,
         ...payload,
         isAthenticated: true,
         loading: false
-      }
+      };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
+    case LOGIN_FAIL:
       localStorage.removeItem('token');
       return {
         ...state,
         token: null,
         isAthenticated: false,
         loading: false
-      }
+      };
     default:
       return state;
   }
-}
+};
